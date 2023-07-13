d8bc5393d05e38eda87d2efd241ff30adf734b07
neyslutrans <neyslutrans@outlook.com>
2023-07-13 15:18:05 +0300

    feat(affinity): Добавить сценарий обработки affinity выгрузок и его модули

---
+++ modules/paths.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from tkinter import filedialog
+
+class PathManager:
+    @staticmethod
+    def open_file_dialog():
+        root = tk.Tk()
+        root.withdraw()
+        file_paths = filedialog.askopenfilenames()
+        return file_paths
+    
+    @staticmethod
+    def save_file_dialog():
+        root = tk.Tk()
+        root.withdraw()
+        file_path = filedialog.asksaveasfilename(defaultextension='.xlsx')
+        return file_path
