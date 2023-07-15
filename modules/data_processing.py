3166211c2800a9982d80f3a8b5bef2bc790dd3c2
neyslutrans <neyslutrans@outlook.com>
2023-07-15 18:26:07 +0300

    feat(modules): Добавить логику обработки reach

--- modules/data_processing.py
+++ modules/data_processing.py
@@ -30,6 +30,60 @@ def affinity_dataframe(df: pd.DataFrame):
         raise Exception("Ошибка при обработке датафрейма") from e
 
 
+# Охват
+def reach_dataframe(df: pd.DataFrame):
+    
+    REMAINS = 50
+
+    try:
+        df.drop(df.columns[[0, 3]], axis=1, inplace=True)
+
+        target = df.iloc[0, 2]
+        header = ['Рекламодатель', 'Дата', f'{target} TVR']
+
+        for i in range(3, len(df.columns)):
+            header.append(df.iloc[2, i])
+
+        df.columns = header
+
+        df.drop([0, 1, 2], axis=0, inplace=True)
+        df.reset_index(drop=True, inplace=True)
+
+        for column in header[2:]:
+            df[column] = df[column].apply(lambda x: x.replace(',', '.')).astype('float64')
+
+        df['Дата'] = df['Дата'].apply(lambda x: x.replace('/', '.'))
+
+        df['Total TVR'] = None
+
+        df.loc[0, 'Total TVR'] = df.loc[0, f'{target} TVR']
+
+        for i in range(1, len(df['Total TVR'])):
+                df.loc[i, 'Total TVR'] = df.loc[i, f'{target} TVR'] + df.loc[i - 1, 'Total TVR']
+                
+
+        df['Остаток'] = df['Total TVR'] % REMAINS
+        df['Шаг'] = df['Total TVR'] - df['Остаток']
+
+        df['Отсечка'] = None
+
+        for i in range(1, len(df['Остаток'])):
+                df.loc[i, 'Отсечка'] = 'yes' if df.loc[i - 1, 'Остаток'] > df.loc[i, 'Остаток'] else 'no'
+
+        df.loc[len(df) - 1, 'Отсечка'] = 'yes'
+
+        # Округляем значения до 4 знаков
+        df = df.round(4)
+
+        return {
+            'dataframe': df,
+            'target': target
+        }
+
+    except Exception as e:
+        raise Exception("Ошибка при обработке датафрейма") from e
+
+
 # Позиционирование
 def positioning_dataframe(df: pd.DataFrame):
     try:
