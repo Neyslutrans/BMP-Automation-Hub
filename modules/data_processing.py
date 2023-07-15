64a169e5871aa338e6e9475282f5e2a54b076a81
neyslutrans <neyslutrans@outlook.com>
2023-07-15 18:20:05 +0300

    feat(positioning): Добавить функцию обработки positioning

--- modules/data_processing.py
+++ modules/data_processing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import re
 
-# affinity
+# Аффинити
 def affinity_dataframe(df: pd.DataFrame):
     try:
         df.drop(df.columns[[0, 3, 4, 7]], axis=1, inplace=True)
@@ -19,6 +19,34 @@ def affinity_dataframe(df: pd.DataFrame):
 
         df['Телеканал'] = df['Телеканал'].apply(get_cleared_channel)
 
+        df = df.round(4)
+
+        return {
+            'dataframe': df,
+            'target': target
+        }
+
+    except Exception as e:
+        raise Exception("Ошибка при обработке датафрейма") from e
+
+
+# Позиционирование
+def positioning_dataframe(df: pd.DataFrame):
+    try:
+        df.drop(df.columns[[0, 4]], axis=1, inplace=True)
+
+        # Определяем целевую аудиторию
+        target = df.iloc[0, 3]
+
+        # Переименовываем столбцы
+        df.columns = ['Блок распространения', 'Телеканал', 'Позиция', target]
+        df.drop([0, 1], axis=0, inplace=True)
+
+        # Форматируем телеканалы и определяем формат чисел
+        df['Телеканал'] = df['Телеканал'].apply(lambda x: get_cleared_channel(x))
+        df[target] = df[target].apply(lambda x: str(x).replace(',', '.').replace('\xa0', '')).astype('float64')
+
+        # Округляем значения до 4 знаков
         df = df.round(4)
 
         return {
