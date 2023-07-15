989b4974f3e85396489bb959acf694ac866177dc
neyslutrans <neyslutrans@outlook.com>
2023-07-15 18:49:14 +0300

    fix(modules): Исправить работу со стилями Excel

--- modules/excel_operations.py
+++ modules/excel_operations.py
@@ -2,7 +2,7 @@ from openpyxl.styles import PatternFill, Font, Alignment
 from openpyxl.styles import numbers
 import openpyxl
 
-def format_sheet(sheet):
+def format_sheet(sheet, type=None):
     try:
         sheet.auto_filter.ref = sheet.dimensions
         sheet.freeze_panes = 'A2'
@@ -13,8 +13,12 @@ def format_sheet(sheet):
             cell.alignment = Alignment(horizontal='left')
 
         percent_format = numbers.FORMAT_PERCENTAGE_00
-        for cell in sheet['E']:
-            cell.number_format = percent_format
+
+        match type:
+            case 'affinity':
+                for cell in sheet['E']:
+                    cell.number_format = percent_format
+
 
     except Exception as e:
         raise Exception("Ошибка при форматировании рабочего листа") from e
